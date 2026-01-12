5e207a9f7baeb5f2b5105d5cb6e6045baa4d9261
kavya-dm <kavya@medianv.com>
2026-01-12 14:23:00 +0530

    Delete Day5-nestjs-Interceptors-and-Custom-Decorators directory

--- Day5-nestjs-Interceptors-and-Custom-Decorators/dist/decorators/log-action.decorator.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const LOG_ACTION_KEY = "log_action";
-export interface LogActionMetadata {
-    action: string;
-    version?: string;
-}
-export declare const LogAction: (action: string, version?: string) => import("@nestjs/common").CustomDecorator<string>;
